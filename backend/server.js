4bb95003193c413a25f052b08ee5103ab674a6e8
DavidsonAPS <davidsonaugusto4@gmail.com>
2026-05-20 18:32:58 -0300

    CRUD inicial funcionando com Supabase

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const db = require('./src/config/db');
 
 const app = express();
 
@@ -8,6 +11,19 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API funcionando!');
+});
+
+app.get('/sensores', async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM sensores');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            erro: 'Erro ao buscar sensores'
+        });
+    }
 });
 
 const PORT = 3000;
